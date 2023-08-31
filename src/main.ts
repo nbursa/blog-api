09b7c7b21297c7c30fed55394f030a8973f55086
Nenad Bursać <nbursa@gmail.com>
2023-08-31 23:21:47 +0200

    fix log errors

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module';
 import * as mongoose from 'mongoose';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-const corsConfig: CorsOptions = {
-  origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-};
+// const corsConfig: CorsOptions = {
+//   origin: 'http://localhost:5173',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credentials: true,
+// };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors(corsConfig);
+  // app.enableCors(corsConfig);
   mongoose.connection.on('connected', () => {
     console.log(
       `Mongoose has connected to ${mongoose.connection.host}:${mongoose.connection.port}`,
