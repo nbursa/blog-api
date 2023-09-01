8850bdf171b23f420a790ed8a2e1474c271449f8
Nenad Bursać <nbursa@gmail.com>
2023-09-01 09:27:37 +0200

    fix log errors

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as mongoose from 'mongoose';
-// import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as dotenv from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+dotenv.config();
 
-// const corsConfig: CorsOptions = {
-//   origin: 'http://localhost:5173',
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//   credentials: true,
-// };
+const corsOrigins = process.env.CORS_ORIGINS
+  ? process.env.CORS_ORIGINS.split(',')
+  : [];
+
+const corsConfig: CorsOptions = {
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: corsOrigins,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsConfig);
+
   app.setGlobalPrefix('api');
-  // app.enableCors(corsConfig);
+
   mongoose.connection.on('connected', () => {
     console.log(
       `Mongoose has connected to ${mongoose.connection.host}:${mongoose.connection.port}`,
     );
   });
 
-  await app.listen(3000, () => {
-    console.log('Application is running on: http://localhost:3000');
+  await app.listen(3001, () => {
+    console.log(`Application is running on: http://localhost:3001`);
   });
 }
 
